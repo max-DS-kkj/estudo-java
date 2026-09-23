a7ce9908dc55600cf6c8632fe79ecfe44ca03d88
max-DS-kkj <l.max@escola.pr.gov.br>
2026-09-23 07:48:05 -0300

    atualicao do programa.java

--- programa.java
+++ programa.java
@@ -1,7 +1,8 @@
-public class programa {
-    public static void main(String args []){
-
-        
+public class programa {  
+    
+    public static void main(String[] args) {
+        System.out.println("Ola java");
 
     }
 }
+
